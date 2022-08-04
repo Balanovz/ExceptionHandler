141b16f580f513dffba3e2d1f4d26a2ac5e962f7
Zaur <86913677+Balanovz@users.noreply.github.com>
2022-08-04 10:55:47 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,50 +1,14 @@
-﻿class Parser
-{
-    public static void Add(double value1, double value2)
-    {
-
-        Console.WriteLine(value1 + value2);
-    }
-
-    public static void Div(double value1, double value2)
-    {
-        if (value2 == 0)
-            throw new DivideByZeroException("Divide by zero isn't possible");
-        else
-            Console.WriteLine(value1 / value2);
-    }
-
-    public static void Mult(double value1, double value2)
-    {
-        Console.WriteLine(value1 * value2);
-    }
-
-    public static void Sub(double value1, double value2)
-    {
-        Console.WriteLine(value1 - value2);
-    }
-}
+using WorkWithExceptions;
 
 class testParser
 {
     public static void Main()
     {
-        try
-        {
-            Parser.Add(4, 2);
-            Parser.Sub(-235.26, 945);
-            Parser.Mult(2, 7);
-            Parser.Div(9, 0);
-        }
-        catch (DivideByZeroException e)
-        {
-            Console.WriteLine("Division by zero isn't possible");
-        }
-        catch
-        {
-            Console.WriteLine("invalid data format");
-        }
-        
-        
+        double firstOperand = 35;
+        double secondOperand = 0;
+        string operation = "9";
+        double result = Parser.CalculateExpression(firstOperand, operation, secondOperand);
+
+        Console.WriteLine(result);
     }
 }
