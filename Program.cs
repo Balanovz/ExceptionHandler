3c266f9135b8430e48224bd73f3a47b275ec37fa
Zaur <86913677+Balanovz@users.noreply.github.com>
2022-08-04 15:14:56 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,9 @@ class testParser
 {
     public static void Main()
     {
-        double firstOperand = 35;
-        double secondOperand = 0;
-        string operation = "9";
-        double result = Parser.CalculateExpression(firstOperand, operation, secondOperand);
+        string expression = Console.ReadLine();
+        double result = Parser.LineProcessing(expression);
 
-        Console.WriteLine(result);
+        Console.WriteLine($"= {result}");
     }
 }
